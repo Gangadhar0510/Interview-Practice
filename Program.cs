907448ef0a49b7b3a2376e7c5374f3e95709aa89
Gangadhar0510 <100117386+Gangadhar0510@users.noreply.github.com>
2025-06-23 18:10:50 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
+public class TokenResponseDto
+{
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; }
+
+    [JsonPropertyName("refresh_token")]
+    public string RefreshToken { get; set; }
+
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; set; }
+
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; set; }
+}
+
 <script>
     var isAuthenticated = '@User.Identity.IsAuthenticated'.toLowerCase() === 'true';
 
